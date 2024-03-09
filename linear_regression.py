9feff639484bd0c48671cd31e19f2dc2cb0c3eee
abarriga <abarriga4242@gmail.com>
2024-03-09 17:05:06 +0100

    feat: progam now working succesfully

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,7 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import pickle
+
 plt.rcParams['figure.figsize'] = (12.0, 9.0)
 
 # Preprocessing Input data
@@ -20,25 +21,28 @@ m = 0
 c = 0
 
 L = 0.1  # The learning Rate
-epochs = 200  # The number of iterations to perform gradient descent
+epochs = 1000  # The number of iterations to perform gradient descent
 
 n = float(len(X)) # Number of elements in X
 
 # Performing Gradient Descent 
 for i in range(epochs): 
     Y_pred = m*X + c  # The current predicted value of Y
-    D_m = (-2/n) * sum(X * (Y - Y_pred))  # Derivative wrt m
-    D_c = (-2/n) * sum(Y - Y_pred)  # Derivative wrt c
+    D_m = (-1/n) * sum(X * (Y - Y_pred))  # Derivative wrt m
+    D_c = (-1/n) * sum(Y - Y_pred)  # Derivative wrt c
     m = m - L * D_m  # Update m
     c = c - L * D_c  # Update c
     
-print (m, c)
+# print (m, c)
 new_m = m / x_mean
 
 print (new_m, c)
 # Plotting the regression line in original scale
 plt.plot(old_X, new_m*old_X+c, color='red')
 plt.show()
-plt.scatter(X, Y)
-plt.plot(X, m*X+c, color='green')
-plt.show()
+# plt.scatter(X, Y)
+# plt.plot(X, m*X+c, color='green')
+# plt.show()
+
+with open('result.pkl', 'wb') as f:  # open a text file
+    pickle.dump([new_m, c], f)
